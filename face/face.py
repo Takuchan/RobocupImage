83bb2d36a24815a4601bb75198f264c7be3eb8ea
Takuchan <takuchanapp@gmail.com>
2023-05-18 23:09:57 +0900

    顔追従して顔の表情を読み取れるようになった

--- face/face.py
+++ face/face.py
@@ -3,7 +3,7 @@ import dlib
 from imutils import face_utils
 
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture('hatsuon.mp4')
 xml_path = "haarcascade_frontalcatface.xml"
 cascade = cv2.CascadeClassifier(xml_path)
 
@@ -22,18 +22,19 @@ while(True):
     lists = cascade.detectMultiScale(resize_frame, minSize=(50, 50))
     for (x,y,w,h) in lists:
         img1 = resize_frame[y:y+h,x:x+w]
+        img1 = cv2.resize(img1,dsize=(200,200))
         
     #顔検出して、特徴点をマークする初歩段階   
-    faces = face_detector(resize_frame,1)
+    faces = face_detector(img1,1)
     
     for face in faces:
-        landmark = face_predictor(resize_frame, face)
+        landmark = face_predictor(img1, face)
         landmark = face_utils.shape_to_np(landmark)
 
         # ランドマーク描画
         for (i, (x, y)) in enumerate(landmark):
             print(i,(x,y))
-            cv2.circle(resize_frame, (x, y), 1, (255, 0, 0), -1)
+            cv2.circle(img1, (x, y), 1, (255, 0, 0), -1)
     
     # cv2.imshow("camera",dst_face)
     # cv2.imshow("camera",resize_frame)
