03001abf86170e69572f345680c0cf38f80ca143
Takuchan <takuchanapp@gmail.com>
2023-05-23 20:36:33 +0900

    68,69がおかしい]

--- face/face.py
+++ face/face.py
@@ -47,17 +47,14 @@ while(True):
     for (x,y,w,h) in lists:
         img1 = resize_frame[y:y+h,x:x+w]
         img1 = cv2.resize(img1,dsize=(200,200))
-    
 
     
     #顔検出して、特徴点をマークする初歩段階   
-    faces = face_detector(img1,1)
-    for index,rect in enumerate(faces):
-
-        # 顔だけ切り出す
-        rectWidth = rect.width()
-        rectHeight = rect.height()
-        rectCenter = rect.center()
+    faces = face_detector(img1,1)   
+    for face in faces:
+        rectWidth = face.width()
+        rectHeight = face.height()
+        rectCenter = face.center()
         x_start = rectCenter.x - rectWidth
         x_end = x_start + rectWidth*2
         y_start = rectCenter.y - rectHeight
@@ -66,15 +63,18 @@ while(True):
 
         # 顔の角度を修正
         points = []
-        for point in face_predictor(img1, rect).parts():
+        for point in face_predictor(img1, face).parts():
             points.append([int(point.x), int(point.y)])
-        x_diff = points[45][0] - points[36][0]
-        y_diff = points[45][1] - points[36][1]
+        x_diff = points[0][0] - points[36][0]
+        y_diff = points[0][1] - points[36][1]
         angle = math.degrees(math.atan2(y_diff, x_diff))
         rotated_im = fitting_rotated_image(face_im, angle)
 
+        cv2.imshow("cereter",rotated_im)
+
         # 回転後の画像で顔検出して画像保存
         rotated_rects = face_detector(rotated_im, 1)
+        print(rotated_rects)
         if len(rotated_rects) == 0:
             print('顔が抽出されませんでした')
             continue
@@ -85,30 +85,29 @@ while(True):
         y_start = rotated_rect.top()
         y_end = rotated_rect.bottom()
         cropped_im = rotated_im[y_start:y_end, x_start:x_end]
-        # cv2.imwrite('face_'+ str(index) +'.jpg', cropped_im)
 
 
-    for face in faces:
-        landmark = face_predictor(img1, face)
+
+        landmark = face_predictor(cropped_im, face)
         landmark = face_utils.shape_to_np(landmark)
 
         # ランドマーク描画とデータを保存する
-        coordinate = []
+        # coordinate = []
         for (i, (x, y)) in enumerate(landmark):
             # print(i,(x,y))
-            cv2.circle(img1, (x, y), 1, (255, 0, 0), -1)
+            cv2.circle(cropped_im, (x, y), 1, (255, 0, 0), -1)
             tempCordinate = faceModel.singlePointFaceModel()
             tempCordinate.set_single_point(facePointNumber=i,x_p=x,y_p=y)
-            coordinate.append(tempCordinate)
+            # coordinate.append(tempCordinate)
     alldata = faceModel.faceAllDataModel()
-    alldata.set_value(list_data=coordinate)
+    # alldata.set_value(list_data=coordinate)
     faceallData.append(alldata)
 
 
-    #画面表示
-    cv2.imshow("Realtime Screen",img1)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+    # #画面表示
+    # cv2.imshow("Realtime Screen",cropped_im)
+    # if cv2.waitKey(1) & 0xFF == ord('q'):
+    #     break
 
 
 with open("finish_data.csv","w") as f:
