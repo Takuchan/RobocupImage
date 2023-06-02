b6f6592d8d7f7988fa172673680ecb29bb5d3510
Takuchan <takuchanapp@gmail.com>
2023-06-02 12:52:38 +0900

    顔正面に向けたい#1

--- face/front_force_face.py
+++ face/front_force_face.py
@@ -22,6 +22,39 @@ class front_force_face:
 
         faces = face_detector(img_gry, 1)
 
+
+        # 各顔について処理を行う
+        for face in faces:
+            # 顔のランドマーク（特徴点）を検出
+            landmarks =face_predictor(img_gry, face)
+
+            # 目の座標を取得
+            left_eye = (landmarks.part(36).x, landmarks.part(36).y)
+            right_eye = (landmarks.part(45).x, landmarks.part(45).y)
+
+            # 目の中心座標を計算
+            center_x = int((left_eye[0] + right_eye[0]) / 2)
+            center_y = int((left_eye[1] + right_eye[1]) / 2)
+
+            # 顔の向きを計算
+            angle = -cv2.fastAtan2(right_eye[1] - left_eye[1], right_eye[0] - left_eye[0])
+
+            # 画像の中心座標を計算
+            image_height, image_width = img_gry.shape[:2]
+            image_center = (image_width // 2, image_height // 2)
+
+            # アフィン変換行列を作成して顔を正面に向ける
+            rotation_matrix = cv2.getRotationMatrix2D(image_center, angle, 1.0)
+            rotated_image = cv2.warpAffine(img_gry, rotation_matrix, (image_width, image_height), flags=cv2.INTER_LINEAR)
+
+            # 顔を正面に向けた画像を表示
+            cv2.imshow('Rotated Image', rotated_image)
+            cv2.waitKey(0)
+
+        # ウィンドウを閉じる
+        cv2.destroyAllWindows()
+
+
         # 検出した全顔に対して処理
         for face in faces:
             # 顔のランドマーク検出
@@ -32,6 +65,9 @@ class front_force_face:
             # ランドマーク描画
             for (i, (x, y)) in enumerate(landmark):
                 cv2.circle(img, (x, y), 1, (255, 0, 0), -1)
+            
+        img1 = cv2.imread('sozai/arimura_yoko1.jpg')
+        img1_gry = cv2.cvtColor()
 
         cv2.imshow('sample', img)
         cv2.waitKey(0)
