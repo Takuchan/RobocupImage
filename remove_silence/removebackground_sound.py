36368243450db01187457d311a94848bbf3c6263
takuchan <takuchanapp@gmail.com>
2023-06-07 19:58:00 +0900

    show waves from music

--- remove_silence/removebackground_sound.py
+++ remove_silence/removebackground_sound.py
@@ -6,40 +6,43 @@ import matplotlib.pyplot as plt
 device_list = sd.query_devices()
 print(device_list)
 
-sd.default.device = [0, 1] # Input, Outputデバイス指定
+sd.default.device = [0, 1]  # Input, Outputデバイス指定
 
-def callback(indata, frames, time, status):
-    # indata.shape=(n_samples, n_channels)
-    global plotdata
-    data = indata[::downsample, 0]
-    shift = len(data)
-    plotdata = np.roll(plotdata, -shift, axis=0)
-    plotdata[-shift:] = data
+class rmbackgroundsound:
+    def callback(self, indata, frames, time, status):
+        # indata.shape=(n_samples, n_channels)
+        data = indata[::self.downsample, 0]
+        shift = len(data)
+        self.plotdata = np.roll(self.plotdata, -shift, axis=0)
+        self.plotdata[-shift:] = data
+
+    def update_plot(self, frame):
+        """This is called by matplotlib for each plot update."""
+        self.line.set_ydata(self.plotdata)
+        return self.line,
+
+    def main(self):
+        self.downsample = 10
+        length = int(1000 * 44100 / (1000 * self.downsample))
+        self.plotdata = np.zeros((length))
+
+        fig, ax = plt.subplots()
+        self.line, = ax.plot(self.plotdata)
+        ax.set_ylim([-1.0, 1.0])
+        ax.set_xlim([0, length])
+        ax.yaxis.grid(True)
+        fig.tight_layout()
+
+        stream = sd.InputStream(
+            channels=1,
+            dtype='float32',
+            callback=self.callback
+        )
+        ani = FuncAnimation(fig, self.update_plot, interval=30, blit=True)
+        with stream:
+            plt.show()
 
 
-def update_plot(frame):
-    """This is called by matplotlib for each plot update.
-    """
-    global plotdata
-    line.set_ydata(plotdata)
-    return line,
-
-downsample = 10
-length = int(1000 * 44100 / (1000 * downsample))
-plotdata = np.zeros((length))
-
-fig, ax = plt.subplots()
-line, = ax.plot(plotdata)
-ax.set_ylim([-1.0, 1.0])
-ax.set_xlim([0, length])
-ax.yaxis.grid(True)
-fig.tight_layout()
-
-stream = sd.InputStream(
-        channels=1,
-        dtype='float32',
-        callback=callback
-    )
-ani = FuncAnimation(fig, update_plot, interval=30, blit=True)
-with stream:
-    plt.show()
+if __name__ == "__main__":
+    rbs = rmbackgroundsound()
+    rbs.main()
