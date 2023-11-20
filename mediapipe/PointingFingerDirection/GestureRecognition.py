f4be64180481be82f89cb0d5b5974db6dfcf535b
takuchan <takuchanapp@gmail.com>
2023-11-20 11:57:58 +0900

    指の形を取得して座標を取得するようにプログラムを変更

--- mediapipe/PointingFingerDirection/GestureRecognition.py
+++ mediapipe/PointingFingerDirection/GestureRecognition.py
@@ -1,23 +1,45 @@
-import mediapipe as mp
-from mediapipe.tasks import python
-from mediapipe.tasks.python import vision
-
 import cv2
+import mediapipe as mp
 
-capture = cv2.VideoCapture(0)
+mp_drawing = mp.solutions.drawing_utils
+mp_drawing_styles = mp.solutions.drawing_styles
+mp_hands = mp.solutions.hands
 
-# MediaPipe Processing
-# base_options = python.BaseOptions(model_asset_path='gesture_recognizer.task')
-# options = vision.GestureRecognizerOptions(base_options=base_options)
-# recognizer = vision.GestureRecognizer.create_from_options(options)
+# Webカメラから入力
+cap = cv2.VideoCapture(0)
+with mp_hands.Hands(
+    model_complexity=0,
+    min_detection_confidence=0.5,
+    min_tracking_confidence=0.5) as hands:
+    while cap.isOpened():
+        success, image = cap.read()
+        if not success:
+            print("Ignoring empty camera frame.")
+            continue
+        image.flags.writeable = False
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        results = hands.process(image)
 
+        # 検出された手の骨格をカメラ画像に重ねて描画
+        image.flags.writeable = True
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+        if results.multi_hand_landmarks:
+            for hand_landmarks in results.multi_hand_landmarks:
+                # 手の各指の関節の座標を表示
+                for idx, landmark in enumerate(hand_landmarks.landmark):
+                    height, width, _ = image.shape
+                    cx, cy = int(landmark.x * width), int(landmark.y * height)
+                    cv2.putText(image, f"{idx}: ({cx}, {cy})", (cx, cy), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1, cv2.LINE_AA)
 
+                mp_drawing.draw_landmarks(
+                    image,
+                    hand_landmarks,
+                    mp_hands.HAND_CONNECTIONS,
+                    mp_drawing_styles.get_default_hand_landmarks_style(),
+                    mp_drawing_styles.get_default_hand_connections_style())
 
+        cv2.imshow('MediaPipe Hands', cv2.flip(image, 1))
+        if cv2.waitKey(5) & 0xFF == 27:
+            break
 
-while True:
-    ret, frame = capture.read()
-    cv2.imshow('frame',frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-capture.release()
-cv2.destroyAllWindows()
+cap.release()
